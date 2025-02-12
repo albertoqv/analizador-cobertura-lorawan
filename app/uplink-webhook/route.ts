dedfea9e9ffd820a4115e8547e60c244e70b6db9
Alberto <albertoqv19@gmail.com>
2025-02-12 21:06:38 +0100

    save in supabase

--- app/uplink-webhook/route.ts
+++ app/uplink-webhook/route.ts
@@ -1,6 +1,18 @@
 import { NextResponse } from 'next/server'
+import { createClient } from '@/utils/supabase/server';
  
 export async function POST(request: Request) {
-    console.log("📡 Datos del cuerpo:", await request.text())
-    return NextResponse.json({ hola: 'Hola' }, { status: 200 })
+    const bodyStr = await request.text()
+    const bodyObj = JSON.parse(bodyStr)
+    const payload = bodyObj.uplink_message.decoded_payload
+    console.log("📡 Datos del cuerpo:", payload)
+    const supabase = await createClient();
+    const { data: notes } = await supabase.from("quality_measure").insert(
+        {
+            latitude: payload.lat,
+            longitud: payload.lon,
+            quality: "100"
+        }
+    );
+    return NextResponse.json({ success: true }, { status: 200 })
 }
