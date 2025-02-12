c82b88ac2003c5726376e8147dc99fc9d933bacd
Alberto <albertoqv19@gmail.com>
2025-02-12 20:29:58 +0100

    improving body logs

--- app/uplink-webhook/route.ts
+++ app/uplink-webhook/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
  
 export async function POST(request: Request) {
-    console.log(request.body)
+    console.log("📡 Datos del cuerpo:", JSON.stringify(request.body, null, 2))
     return NextResponse.json({ hola: 'Hola' }, { status: 200 })
 }
