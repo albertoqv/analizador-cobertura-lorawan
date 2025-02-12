06343e43f8e97d2cb8f2f2bf6e49b550af8befe7
Alberto <albertoqv19@gmail.com>
2025-02-12 20:40:06 +0100

    log text

--- app/uplink-webhook/route.ts
+++ app/uplink-webhook/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server'
  
 export async function POST(request: Request) {
-    console.log("📡 Datos del cuerpo:", JSON.stringify(request.body, null, 2))
+    console.log("📡 Datos del cuerpo:", request.text)
     return NextResponse.json({ hola: 'Hola' }, { status: 200 })
 }
