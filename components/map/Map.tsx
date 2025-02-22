8c82383aaff793e8290bfa16a96d5293e52e2c3a
Alberto <albertoqv19@gmail.com>
2025-02-22 12:13:13 +0100

    try to resolve map errors

--- components/map/Map.tsx
+++ components/map/Map.tsx
@@ -8,6 +8,7 @@ import Map, { ViewState } from "react-map-gl/mapbox";
 import "mapbox-gl/dist/mapbox-gl.css";
 
 mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN || "";
+console.log("🔍 MAPBOX TOKEN:", process.env.NEXT_PUBLIC_MAPBOX_TOKEN);
 
 const API_URL = "/api/quality_points";
 
